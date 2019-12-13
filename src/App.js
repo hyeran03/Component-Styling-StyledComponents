01611155dc8b465502136114fdb155e6f192b670
hyeran03 <user@email.mail>
2019-12-13 16:11:04 +0900

    Styled-components  TaggedTemplateLiteral

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { css } from "styled-components";
+// tagged templace literal 이 되기위해서는 {css} 를 불러와야한다
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color};
+  border-radius: 50%;
+  ${props =>
+    props.huge &&
+    // template literal에서는 이 안에 ${props}를 사용할수 없으므로 {css}를 사용 tagged로 바꿈
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Circle color="black" />
+      <Circle color="blue" huge />
+    </>
   );
 }
 
 export default App;
+
+// 확장프로그램인 vscode-styled Component를 사용하면 더 깔금해진다
